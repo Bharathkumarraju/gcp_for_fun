bb6f399ec55eac9ae2d5c663f0e6940446cec6ed
Bharath <bharath@hooq.tv>
2021-06-11 16:55:47 +0800

    ingress kubernetes resource

--- 2.GKE/GKE_Networking/Services/Group_services_using_Ingress_resource/demo.sh
+++ 2.GKE/GKE_Networking/Services/Group_services_using_Ingress_resource/demo.sh
@@ -1,1 +1,22 @@
 A way to group services together using the Ingress resource.
+one most powerful tools to direct the traffic to cluster.
+
+Ingress resource operates one layer higher than the services.
+
+Ingress is not a service or even type service.
+It is a collection of rules that direct external inbound connections to a set of servcices within the cluster.
+
+In GKE an ingress resource exposes these services using a single public IP Address bound to http or https loadbalancer provisioned within the google cloud.
+
+
+On GKE Kubernetes ingress resources are implemented using cloud loadbalancing.
+When you create a ingress resource in your cluster GKE creates HTTP(or) HTTPS Loadbalancer and configures it to route traffic to your application.
+
+ingress service directs traffic to NodePort services or loadbalancer.
+
+
+
+$ kubectl edit ingress [NAME]
+
+$ kubectl replace -f [FILE]
+
