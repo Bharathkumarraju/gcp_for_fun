5338de7c01f05ed501d2d3491a3e6dc135f5e818
Bharath <bharath@hooq.tv>
2021-06-13 05:59:51 +0800

    persistent volumes and storage types

---
+++ 2.GKE/GKE_Persistent_Data_Storage/Volumes/demo.sh
@@ -0,0 +1,15 @@
+Volumes
+PersistentVolumes
+
+Volumes:
+--------->
+Volumes are the method by which you attach a storage to a Pod.
+Some volumes are ephemeral.
+Some volumes are persistent
+
+
+Persistent Storage options:
+------------------------------>
+block storage (or) networked file systems
+in GKE its compute engine persistent disks.
+
---
+++ 2.GKE/GKE_Persistent_Data_Storage/volume_types_2/demo.sh
@@ -0,0 +1,5 @@
+Secret and ConfigMap Volumes are ephemeral.
+
+PersistentVolumes
+PersistentVolumeClaims
+
